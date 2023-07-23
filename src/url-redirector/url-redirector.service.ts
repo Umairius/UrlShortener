d4d0ab000711f1a11c6e43e918eaea389e70c094
Umairius <your-email@example.com>
2023-07-23 16:54:25 +0500

    redis added

--- src/url-redirector/url-redirector.service.ts
+++ src/url-redirector/url-redirector.service.ts
@@ -1,34 +1,53 @@
 import { Injectable } from '@nestjs/common';
-import {InjectModel} from '@nestjs/mongoose';
-import {Model} from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { Url } from 'src/Models/url-entity';
+
+import Redis from 'ioredis';
+const redisClient = new Redis(); // Assuming Redis server is running locally on the default port
 
 @Injectable()
 export class UrlRedirectorService {
-  
   constructor(@InjectModel('Url') private readonly urlEntity: Model<Url>) {}
 
-  redirectToOriginal( shortUrl:string ): Promise<string>{
-    console.log(shortUrl)
-      return  new Promise((resolve, reject) => {
-        this.urlEntity.findOne({shortUrl : shortUrl})
-          .then((url) => {
-            if(url){
-              console.log(url)
-              url.clicks++;
-              url.save();
-              resolve(url.longUrl);
-            }
-            else{
-              reject("No such url found");
-            }
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      }
+  async redirectToOriginal(shortUrl: string): Promise<string> {
+    console.log(shortUrl);
 
-      );
+    try {
+      // Use the getOrSetCache function to fetch the long URL from cache or database
+      const longUrl = await this.getOrSetCache(shortUrl, async () => {
+        const url = await this.urlEntity.findOne({ shortUrl: shortUrl });
+        if (url) {
+          url.clicks++;
+          await url.save();
+          return url.longUrl;
+        } else {
+          throw new Error("No such url found");
+        }
+      });
+
+      return longUrl;
+    } catch (error) {
+      throw error;
+    }
   }
 
+  async getOrSetCache(key: string, cb: () => Promise<string>): Promise<string> {
+    // Check if the key exists in the cache
+    const cachedData = await redisClient.get(key);
+
+    if (cachedData) {
+      // If it does, return the data
+      return cachedData;
+    } else {
+      // If it does not, call the callback function
+      const data = await cb();
+
+      // Save the data in the cache
+      await redisClient.set(key, data);
+
+      // Return the data
+      return data;
+    }
+  }
 }
