db2e6c26dbcc2bf9d07311042b8eab6e7ac2386b
Umairius <your-email@example.com>
2023-07-24 17:32:52 +0500

    added redirect for short url input

--- src/url-redirector/url-redirector.controller.ts
+++ src/url-redirector/url-redirector.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body, Get, Redirect,Query, Param, ValidationPipe, UsePipes} from '@nestjs/common';
+import { Controller, Post, Body, Get, Redirect,Query, Param, ValidationPipe, UsePipes, Res} from '@nestjs/common';
 
 import { UrlRedirectorService } from './url-redirector.service';
+import { Response } from 'express';
 
 
 @Controller('redirect')
@@ -9,16 +10,26 @@ export class UrlRedirectorModuleController {
 
   @UsePipes(new ValidationPipe())
   @Get("/:shortUrl")
-  async handleRedirect(@Param("shortUrl") shortUrl: string) {
+  async handleRedirect(@Param("shortUrl") shortUrl: string, @Res() res: Response) {
    console.log(shortUrl)
 
-    let res =  await this.urlRedirectorService.redirectToOriginal(shortUrl);
-
-    return {
-      Url: res
-    };
-    };
+   try {
+    const longUrl = await this.urlRedirectorService.redirectToOriginal(shortUrl);
+    if (longUrl) {
+      // Redirect the user to the long URL
+      return res.redirect(301, longUrl);
+    } else {
+      // If the short URL is not found, return a 404 Not Found response or any appropriate error response
+      return res.status(404).send('Short URL not found');
+    }
+  } catch (error) {
+    // Handle any error that might occur during the redirection process
+    return res.status(500).send('An error occurred');
   }
+}
+
+    };
+  
 
 
 
--- src/url-redirector/url-redirector.service.ts
+++ src/url-redirector/url-redirector.service.ts
@@ -4,7 +4,7 @@ import { Model } from 'mongoose';
 import { Url } from 'src/Models/url-entity';
 
 import Redis from 'ioredis';
-const redisClient = new Redis(); // Assuming Redis server is running locally on the default port
+const redisClient = new Redis();
 
 @Injectable()
 export class UrlRedirectorService {
@@ -14,7 +14,6 @@ export class UrlRedirectorService {
     console.log(shortUrl);
 
     try {
-      // Use the getOrSetCache function to fetch the long URL from cache or database
       const longUrl = await this.getOrSetCache(shortUrl, async () => {
         const url = await this.urlEntity.findOne({ shortUrl: shortUrl });
         if (url) {
@@ -33,20 +32,16 @@ export class UrlRedirectorService {
   }
 
   async getOrSetCache(key: string, cb: () => Promise<string>): Promise<string> {
-    // Check if the key exists in the cache
+
     const cachedData = await redisClient.get(key);
 
     if (cachedData) {
-      // If it does, return the data
+  
       return cachedData;
     } else {
-      // If it does not, call the callback function
+  
       const data = await cb();
-
-      // Save the data in the cache
       await redisClient.set(key, data);
-
-      // Return the data
       return data;
     }
   }
